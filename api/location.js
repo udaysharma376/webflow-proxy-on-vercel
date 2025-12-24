958ab794b9b65702b3270c9628920163eae8db62
udaysharma376 <sharmaud45@gmail.com>
2025-12-24 17:37:05 +0530

    Update location.js

--- api/location.js
+++ api/location.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://api.webflow.com/v2/collections/69423901ea3cd5eb9716cb8b/items?slug=${slug}`,
+      `https://api.webflow.com/v2/collections/69423901ea3cd5e0b9716cb8/items?slug=${slug}`,
       {
         headers: {
           Authorization: `Bearer ${process.env.WEBFLOW_TOKEN}`,
