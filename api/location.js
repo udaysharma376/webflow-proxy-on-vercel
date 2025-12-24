02a6178afce704063b079aa092195587298271e5
udaysharma376 <sharmaud45@gmail.com>
2025-12-24 17:48:16 +0530

    Update location.js

--- api/location.js
+++ api/location.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  const { slug } = req.query;
+  const { id } = req.query;
 
   try {
     const response = await fetch(
-      `https://api.webflow.com/v2/collections/69423901ea3cd5e0b9716cb8/items?slug=${slug}`,
+      `https://api.webflow.com/v2/collections/6943dcb5680f71517cf50660/items/${id}`,
       {
         headers: {
           Authorization: `Bearer ${process.env.WEBFLOW_TOKEN}`,
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
 
     const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(data);
+    res.status(200).json(data.fieldData);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch location data" });
+    res.status(500).json({ error: "Failed to fetch service data" });
   }
 }
